661cc0f37d229f4b5bfb0cbf60ddf7bb00354d5a
closesttopurple <appleapples3500@gmail.com>
2025-08-07 15:45:49 -0400

    bug

--- astro.config.mjs
+++ astro.config.mjs
@@ -18,14 +18,14 @@ export default defineConfig({
 
   site: "https://closesttopurple.org/ths",
   integrations: [tailwind(), sitemap(), icon(), mdx(),react()],
-  output: "server",
-  adapter: cloudflare(),
-      resolve: {
-      // Use react-dom/server.edge instead of react-dom/server.browser for React 19.
-      // Without this, MessageChannel from node:worker_threads needs to be polyfilled.
+  vite: {
+    resolve: {
       alias: import.meta.env.PROD && {
-        "react-dom/server": "react-dom/server.edge",
+                "react-dom/server": "react-dom/server.edge",
       },
     },
   },
-);
+  output: "server",
+  adapter: cloudflare(),
+
+});
